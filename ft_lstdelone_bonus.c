a491c014a68ba9f9270ea5ee7f77908b350cd760
Francois Moulin <fmoulin@f6r12s13.clusters.42paris.fr>
2025-05-12 13:08:47 +0200

    lstdelone done

--- ft_lstdelone_bonus.c
+++ ft_lstdelone_bonus.c
@@ -3,16 +3,41 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: francoismoulin <francoismoulin@student.    +#+  +:+       +#+        */
+/*   By: fmoulin <fmoulin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 14:37:43 by francoismou       #+#    #+#             */
-/*   Updated: 2025/05/09 14:45:37 by francoismou      ###   ########.fr       */
+/*   Updated: 2025/05/12 13:07:39 by fmoulin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstdelone(t_list *lst, void (*del)(void *))
+void del(void *content)
 {
-    
+	free(content);
 }
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+    if (!lst || !del)
+		return;
+	del(lst->content);
+	free(lst);
+}
+
+// #include <stdio.h>
+
+// int main(void)
+// {
+// 	t_list *lst;
+// 	lst = malloc(sizeof(t_list));
+// 	if (lst == NULL)
+// 		return (0);
+// 	lst->content = ft_strdup("hello world");
+// 	lst->next = NULL;
+	
+// 	printf("%s\n", (char *)lst->content);
+// 	ft_lstdelone(lst, del);
+// 	printf("%s\n", (char *)lst->content); /*segfault*/
+// 	return (0);
+// }
